d301042f9a74bfe5416f8c1a582c4526782736d1
Carolista <cjones.renaissancewoman@gmail.com>
2023-07-24 15:58:59 -0400

    updated solution to match recording

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
-window.addEventListener('load', function() {
-
+window.addEventListener('load', function () {
   const container = document.getElementById('container');
 
   /*** FETCH WITH TRADITIONAL SYNTAX (WITHOUT BONUS MISSIONS) ***/
 
   // fetch('https://handlers.education.launchcode.org/static/astronauts.json').then(function(response) {
   //   response.json().then(function(data) {
-  //   console.log(data);
+  //     console.log(data);
 
   //     for (let i=0; i < data.length; i++) {
   //       let astronaut = data[i];
@@ -27,28 +26,29 @@ window.addEventListener('load', function() {
   //   });
   // });
 
-
   /*** FETCH WITH MODERN ASYNC/AWAIT SYNTAX & BONUS MISSIONS ***/
 
   async function fetchAndDisplayAstronauts() {
-    let response = await fetch('https://handlers.education.launchcode.org/static/astronauts.json');
+    let response = await fetch(
+      'https://handlers.education.launchcode.org/static/astronauts.json'
+    );
     let data = await response.json();
-    
+
+    // Bonus mission 1
+    data.sort(function (a, b) {
+      return a.hoursInSpace < b.hoursInSpace ? 1 : -1;
+    });
+
     // Bonus mission 3
     const count = document.getElementById('count');
     count.innerHTML = `These ${data.length} extraordinary people are a few of the handful of humankind who have had the privilege of looking down on our beautiful planet from the quiet darkness of outer space.`;
 
-    // Bonus mission 1
-    data.sort(function(a, b) {
-      return a.hoursInSpace < b.hoursInSpace ? 1 : -1;
-    });
-
-    for (let i=0; i < data.length; i++) {
+    for (let i = 0; i < data.length; i++) {
       let astronaut = data[i];
 
       // Bonus mission 2
-      let activeClass = astronaut.active ? "green" : ""; // ternary
-      
+      let activeClass = astronaut.active ? 'active' : ''; // ternary
+
       container.innerHTML += `
         <div class="astronaut">
           <div class="bio">
@@ -56,12 +56,12 @@ window.addEventListener('load', function() {
             <ul>
               <li>Hours in space: ${astronaut.hoursInSpace}</li>
               <li class="${activeClass}">Active: ${astronaut.active}</li>
-              <li>Skills: ${astronaut.skills.join(", ")}</li>
+              <li>Skills: ${astronaut.skills.join(', ')}</li>
             </ul>
           </div>
           <img class="avatar" src="${astronaut.picture}">
         </div>
-      `
+      `;
     }
   }
 
