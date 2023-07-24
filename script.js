f9f39a0ca720b3dd6b206ad374f3fe86eee147b4
Carolista <cjones.renaissancewoman@gmail.com>
2023-07-24 14:35:25 -0400

    updated solution

--- script.js
+++ script.js
@@ -1,19 +1,12 @@
 window.addEventListener('load', function() {
 
-  const count = document.getElementById('count');
   const container = document.getElementById('container');
 
-  /*** FETCH WITH TRADITIONAL SYNTAX ***/
+  /*** FETCH WITH TRADITIONAL SYNTAX (WITHOUT BONUS MISSIONS) ***/
 
   // fetch('https://handlers.education.launchcode.org/static/astronauts.json').then(function(response) {
   //   response.json().then(function(data) {
   //   console.log(data);
-
-  //     count.innerHTML = `There are ${data.length} astronauts!`;
-
-  //     data.sort(function(a, b) {
-  //       return a.hoursInSpace < b.hoursInSpace ? 1 : -1;
-  //     });
 
   //     for (let i=0; i < data.length; i++) {
   //       let astronaut = data[i];
@@ -23,7 +16,7 @@ window.addEventListener('load', function() {
   //             <h3>${astronaut.firstName} ${astronaut.lastName}</h3>
   //             <ul>
   //               <li>Hours in space: ${astronaut.hoursInSpace}</li>
-  //               <li class="${astronaut.active ? "green" : ""}">Active: ${astronaut.active}</li>
+  //               <li>Active: ${astronaut.active}</li>
   //               <li>Skills: ${astronaut.skills.join(", ")}</li>
   //             </ul>
   //           </div>
@@ -32,29 +25,37 @@ window.addEventListener('load', function() {
   //       `
   //     }
   //   });
-  // })
+  // });
 
-  /*** FETCH WITH MODERN ASYNC/AWAIT SYNTAX ***/
+
+  /*** FETCH WITH MODERN ASYNC/AWAIT SYNTAX & BONUS MISSIONS ***/
 
   async function fetchAndDisplayAstronauts() {
     let response = await fetch('https://handlers.education.launchcode.org/static/astronauts.json');
     let data = await response.json();
     
+    // Bonus mission 3
+    const count = document.getElementById('count');
     count.innerHTML = `These ${data.length} extraordinary people are a few of the handful of humankind who have had the privilege of looking down on our beautiful planet from the quiet darkness of outer space.`;
 
+    // Bonus mission 1
     data.sort(function(a, b) {
       return a.hoursInSpace < b.hoursInSpace ? 1 : -1;
     });
 
     for (let i=0; i < data.length; i++) {
       let astronaut = data[i];
+
+      // Bonus mission 2
+      let activeClass = astronaut.active ? "green" : ""; // ternary
+      
       container.innerHTML += `
         <div class="astronaut">
           <div class="bio">
             <h3>${astronaut.firstName} ${astronaut.lastName}</h3>
             <ul>
               <li>Hours in space: ${astronaut.hoursInSpace}</li>
-              <li class="${astronaut.active ? "green" : ""}">Active: ${astronaut.active}</li>
+              <li class="${activeClass}">Active: ${astronaut.active}</li>
               <li>Skills: ${astronaut.skills.join(", ")}</li>
             </ul>
           </div>
